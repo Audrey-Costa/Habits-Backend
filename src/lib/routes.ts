3179c5bebe1e94362af91b7f5bdd8e165d7fe683
Audrey-Costa <audreycosta@nano.ufrj.br>
2023-05-09 23:04:57 -0300

    feat(Routes): Adds get habits routes

--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -7,6 +7,10 @@ interface habitsPostRequest {
   weekDays: number[]
 }
 
+interface dayGetRequest {
+  date: string
+}
+
 export async function appRoutes(app: FastifyInstance){
   app.post('/habits', async (request) => {
     const { title, weekDays } = <habitsPostRequest>request.body;
@@ -28,5 +32,43 @@ export async function appRoutes(app: FastifyInstance){
     });
 
     return;
+  });
+
+  app.get('/day', async (request) => {
+    const { date } = <dayGetRequest>request.query;
+    const week_day = dayjs(date).get('day');
+    const dayTime = dayjs(date).toDate();
+
+    const possibleHabits = await prisma.habit.findMany({
+      where: {
+        createdAt: {
+          lte: dayTime
+        },
+        HabitWeekDay:{
+          some: {
+            week_day
+          }
+        }
+      }
+    });
+
+    const day = await prisma.day.findUnique({
+      where: {
+        date: dayTime
+      },
+      include: {
+        dayHabits: true
+      }
+    });
+
+    const completedHabits = day?.dayHabits.map((habit) => {
+      return habit.habit_id;
+    });
+
+
+    return {
+      possibleHabits,
+      completedHabits
+    }
   });
 }
